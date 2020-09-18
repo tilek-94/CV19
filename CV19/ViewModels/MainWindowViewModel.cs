d2b6195747c9d7848b0562ede0e940c4fa391930
tilek-94 <63234535+tilek-94@users.noreply.github.com>
2020-09-18 23:57:08 +0600

    Add project files.

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.ViewModels
+{
+  internal  class MainWindowViewModel:ViewModel
+    {
+        #region Зоголовка окна
+        private string _Title="Анализ статистики CV19";
+        public string Title
+        {
+            get=>_Title;
+            set => Set(ref _Title,value);
+        }
+        #endregion
+    }
+}
